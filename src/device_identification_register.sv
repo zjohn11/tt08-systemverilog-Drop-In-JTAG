18aafb807d8b2d8b8ef7d4d59c57c4bfa56a41eb
zjohn11 <122994816+zjohn11@users.noreply.github.com>
2024-08-26 18:58:25 -0500

    Update device_identification_register.sv

--- src/device_identification_register.sv
+++ src/device_identification_register.sv
@@ -11,16 +11,16 @@ module device_identification_register (
 
 localparam device_id = `DEVICE_ID;
 logic [32:0] shift_reg;
+assign shift_reg[32] = tdi;
 assign tdo = shift_reg[0];
 
 genvar i;
 for (i = 0; i < 32; i = i + 1) begin
     always @(posedge clockDR) begin
-        shift_reg[32] <= tdi;
         shift_reg[i] <= captureDR ? device_id[i] : shift_reg[i+1];
     end
 end
 
-    assert property (@(negedge clockDR) captureDR |-> shift_reg[0] == 1'b1); // else $error("Violation IEEE 1149.1-2013 12.1.1: LSB of identification code must be 1.");
+    assert property (@(negedge clockDR) captureDR |-> shift_reg[0] == 1'b1); else $error("Violation IEEE 1149.1-2013 12.1.1: LSB of identification code must be 1.");
 
 endmodule // device_identification_register
