a8793cc6f0d8932cb0d369d0a391c729b323b990
zjohn11 <122994816+zjohn11@users.noreply.github.com>
2024-08-29 17:51:59 -0500

    Update project.sv

--- src/project.sv
+++ src/project.sv
@@ -20,6 +20,7 @@ module tt_um_jtag (
   //assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
+    assign uo_out[7:3] = 5'b0;
 
   // List all unused inputs to prevent warnings
     wire _unused = &{ena, ui_in[7:4], uio_in[7:0], 1'b0};
