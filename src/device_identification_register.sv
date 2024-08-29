ad1872ba734ee3567498b921bd4a10341434db5d
zjohn11 <122994816+zjohn11@users.noreply.github.com>
2024-08-29 15:04:48 -0500

    Update device_identification_register.sv

--- src/device_identification_register.sv
+++ src/device_identification_register.sv
@@ -21,6 +21,6 @@ for (i = 0; i < 32; i = i + 1) begin
     end
 end
 
-    assert property (@(negedge clockDR) captureDR |-> shift_reg[0] == 1'b1); else $error("Violation IEEE 1149.1-2013 12.1.1: LSB of identification code must be 1.");
+    //assert property (@(negedge clockDR) captureDR |-> shift_reg[0] == 1'b1); else $error("Violation IEEE 1149.1-2013 12.1.1: LSB of identification code must be 1.");
 
 endmodule // device_identification_register
